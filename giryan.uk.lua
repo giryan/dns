9a545a9e756d412d131979c4e80e388aba3a3b6b
giryan <giryan@users.noreply.github.com>
2026-03-26 11:44:43 +0000

    Update giryan.uk.lua

--- giryan.uk.lua
+++ giryan.uk.lua
@@ -9,4 +9,8 @@ a("*.internal", "81.187.12.59")
 cname("pan", "ytn1vh8l6yfd2l2.cname.pangolin.net")
 cname("_acme-challenge.pan.giryan.uk","_acme-challenge.ytn1vh8l6yfd2l2.cname.pangolin.net")
 
+ns("testing", "ns1.pangolin.net")
+ns("testing", "ns2.pangolin.net")
+ns("testing", "ns3.pangolin.net")
+
 -- a("aleph1.internal", "192.168.0.99")
